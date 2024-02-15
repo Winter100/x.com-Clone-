eda204f82a9609a05e0f96a4f4d3f87b7419108e
winter100 <100taeng@gmail.com>
2024-02-15 21:18:48 +0900

    로딩 컴포넌트 추가

---
+++ src/app/_components/loading/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./loading.module.css";
+
+export default function Loading() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+}

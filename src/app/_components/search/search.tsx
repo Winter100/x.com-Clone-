f81e4ef9008494bce64d5258dffe0f28b41edf8c
winter100 <100taeng@gmail.com>
2024-02-19 18:38:46 +0900

    search 배경색 변경

--- src/app/_components/search/search.tsx
+++ src/app/_components/search/search.tsx
@@ -2,15 +2,20 @@
 
 import React from "react";
 import styles from "./search.module.css";
+import Image from "next/image";
 
 export default function Search() {
   return (
     <div className={styles.container}>
       <div className={styles.inputContainer}>
-        <div>로고</div>
+        <div className={styles.search}>
+          <Image src={"/search.svg"} alt="설정" width={18} height={18} />
+        </div>
         <input className={styles.input} type="text" placeholder="검색" />
       </div>
-      <div>설정</div>
+      <div className={styles.setting_div}>
+        <Image src={"/settings.svg"} alt="설정" width={18} height={18} />
+      </div>
     </div>
   );
 }

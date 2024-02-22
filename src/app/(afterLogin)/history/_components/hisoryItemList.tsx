b8cf02e53892169388e6de4a520394a13294e18f
winter100 <100taeng@gmail.com>
2024-02-22 21:14:16 +0900

    댓글페이지 추가

--- src/app/(afterLogin)/history/_components/hisoryItemList.tsx
+++ src/app/(afterLogin)/history/_components/hisoryItemList.tsx
@@ -2,19 +2,8 @@
 
 import Comment from "@/app/_components/comment/comment";
 import Item from "@/app/_components/item/Item";
-import Loading from "@/app/_components/loading/loading";
-import { useHistoryData } from "@/app/util/tanstack-query/useHistory";
-import { useParams } from "next/navigation";
-import React from "react";
 
-export default function HisoryItemList() {
-  const { id } = useParams<{ id: string }>();
-
-  const { data, isLoading, isError, error } = useHistoryData(id);
-
-  if (isLoading) return <Loading />;
-  if (isError) return <p>{error?.message}</p>;
-
+export default function HisoryItemList({ data }: any) {
   const DUMMY = new Array(Math.floor(Math.random() * 5)).fill(1);
   return (
     <div style={{ margin: "auto" }}>
--- src/app/(afterLogin)/history/comment/[id]/page.tsx
+++ src/app/(afterLogin)/history/comment/[id]/page.tsx
@@ -2,14 +2,14 @@
 
 import HistoryTopBar from "../../_components/historyTopBar";
 import HisoryItemList from "../../_components/hisoryItemList";
-import { useParams } from "next/navigation";
-import { useHistoryData } from "@/app/util/tanstack-query/useHistory";
 import Loading from "@/app/_components/loading/loading";
+import { useParams } from "next/navigation";
+import { useKeyword } from "@/app/util/tanstack-query/useKeyword";
 
 export default function Page() {
   const { id } = useParams<{ id: string }>();
 
-  const { data, isLoading, isError, error } = useHistoryData(id);
+  const { data, isLoading, isError, error } = useKeyword(id);
 
   if (isLoading) return <Loading />;
   if (isError) return <p>{error?.message}</p>;
